f1bb7e9542132b82b13ac3b0fd073b1da07a536a
Jackson Argo <jackson@jacksonargo.com>
2020-04-07 01:47:41 -0700

    finally breaking out the goroutines

--- api_server.go
+++ api_server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
@@ -15,6 +16,7 @@ import (
 	"net/http/pprof"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -295,6 +297,14 @@ type Upload struct {
 	UploadType        `json:"upload_type"`
 	*ClickTrackUpload `json:"click_track_upload"`
 	*SheetMusicUpload `json:"sheet_music_upload"`
+	FileName          string `json:"file_name"`
+	FileBytes         []byte `json:"file_bytes"`
+}
+
+type UploadStatus struct {
+	FileName string
+	Code     int
+	Error    string
 }
 
 type ClickTrackUpload struct {
@@ -302,7 +312,6 @@ type ClickTrackUpload struct {
 	Project     string   `json:"project"`
 	PartNames   []string `json:"part_names"`
 	PartNumbers []int    `json:"part_numbers"`
-	FileBytes   []byte   `json:"file_bytes"`
 }
 
 type SheetMusicUpload struct {
@@ -310,8 +319,9 @@ type SheetMusicUpload struct {
 	Project     string   `json:"project"`
 	PartNames   []string `json:"part_names"`
 	PartNumbers []int    `json:"part_numbers"`
-	FileBytes   []byte   `json:"file_bytes"`
 }
+
+const UploadsTimeout = 10 * time.Second
 
 func (x *ApiServer) Upload(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
@@ -331,7 +341,72 @@ func (x *ApiServer) Upload(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// do something with the documents
+	if len(documents) == 0 {
+		return // nothing to do
+	}
+
+	// we'll handle the uploads in goroutines, since these make outgoing http requests to object storage.
+	var wg sync.WaitGroup
+	ctx, cancel := context.WithTimeout(context.Background(), UploadsTimeout)
+	defer cancel()
+	wg.Add(len(documents))
+	statuses := make(chan UploadStatus, len(documents))
+	for _, upload := range documents {
+		switch upload.UploadType {
+		case UploadTypeClickTrack:
+			go func() {
+				defer wg.Done()
+				statuses <- x.handleClickTrack(ctx, upload)
+			}()
+		case UploadTypeSheetMusic:
+			go func() {
+				defer wg.Done()
+				statuses <- x.handleSheetMusic(ctx, upload)
+			}()
+		default:
+			wg.Done()
+			statuses <- UploadStatus{
+				FileName: upload.FileName,
+				Code:     http.StatusBadRequest,
+				Error:    "invalid upload type",
+			}
+		}
+	}
+
+	wg.Wait()
+	close(statuses)
+
+	results := make([]UploadStatus, 0, len(documents))
+	for status := range statuses {
+		results = append(results, status)
+	}
+	json.NewEncoder(w).Encode(&results)
+}
+
+func (x *ApiServer) handleClickTrack(ctx context.Context, upload Upload) UploadStatus {
+	select {
+	case <-ctx.Done():
+		return UploadStatus{
+			FileName: upload.FileName,
+			Code:     http.StatusRequestTimeout,
+			Error:    ctx.Err().Error(),
+		}
+	default:
+		panic("Implement me!")
+	}
+}
+
+func (x *ApiServer) handleSheetMusic(ctx context.Context, upload Upload) UploadStatus {
+	select {
+	case <-ctx.Done():
+		return UploadStatus{
+			FileName: upload.FileName,
+			Code:     http.StatusRequestTimeout,
+			Error:    ctx.Err().Error(),
+		}
+	default:
+		panic("Implement me!")
+	}
 }
 
 func (x *ApiServer) Download(w http.ResponseWriter, r *http.Request) {
