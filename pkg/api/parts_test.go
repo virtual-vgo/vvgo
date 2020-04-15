de2a4c0f11709e0e5808e66991556a5ed93e5575
Jackson Argo <jackson@jacksonargo.com>
2020-04-14 23:13:57 -0700

    update tests

--- pkg/api/parts_test.go
+++ pkg/api/parts_test.go
@@ -4,9 +4,10 @@ import (
 	"bytes"
 	"encoding/json"
 	"github.com/stretchr/testify/assert"
+	"github.com/tdewolff/minify/v2"
+	"github.com/tdewolff/minify/v2/html"
 	"github.com/virtual-vgo/vvgo/pkg/parts"
 	"github.com/virtual-vgo/vvgo/pkg/storage"
-	"golang.org/x/net/html"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -118,6 +119,7 @@ func TestPartsHandler_ServeHTTP(t *testing.T) {
 			server.ServeHTTP(recorder, request)
 			gotResp := recorder.Result()
 			gotBody := strings.TrimSpace(recorder.Body.String())
+			t.Logf("Got Body:\n%s\n", gotBody)
 			if expected, got := tt.wants.code, gotResp.StatusCode; expected != got {
 				t.Errorf("expected code %v, got %v", expected, got)
 			}
@@ -133,18 +135,20 @@ func TestPartsHandler_ServeHTTP(t *testing.T) {
 				gotBody = string(gotBytes)
 
 			case mockHTML:
-				wantHTML := html.NewTokenizer(strings.NewReader(mockHTML))
-				gotHTML := html.NewTokenizer(strings.NewReader(gotBody))
-
-				tt.wants.body = ""
-				for token := wantHTML.Next(); token != html.ErrorToken; token = wantHTML.Next() {
-					tt.wants.body += string(bytes.TrimSpace(wantHTML.Raw()))
+				m := minify.New()
+				m.AddFunc("text/html", html.Minify)
+				var gotBuf bytes.Buffer
+				if err := m.Minify("text/html", &gotBuf, strings.NewReader(gotBody)); err != nil {
+					panic(err)
 				}
+				gotBody = gotBuf.String()
 
-				gotBody = ""
-				for token := gotHTML.Next(); token != html.ErrorToken; token = gotHTML.Next() {
-					gotBody += string(bytes.TrimSpace(gotHTML.Raw()))
+
+				var wantBuf bytes.Buffer
+				if err := m.Minify("text/html", &wantBuf, strings.NewReader(tt.wants.body)); err != nil {
+					panic(err)
 				}
+				tt.wants.body = wantBuf.String()
 			}
 
 			assert.Equal(t, tt.wants.body, gotBody, "body")
