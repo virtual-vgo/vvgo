d137f6f58f7f9459ac672149e7ba7c58214e6fb0
jackson <jackson@jacksonargo.com>
2020-04-26 14:17:41 -0700

    add discord auth controller

---
+++ pkg/api/login_test.go
@@ -0,0 +1,12 @@
+package api
+
+import (
+	"net/http"
+)
+
+func noFollow(client *http.Client) *http.Client {
+	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+	return client
+}
