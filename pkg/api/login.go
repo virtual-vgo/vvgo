c64b7fbd7108b06cc972dcf42ce10e4add91c12a
jackson <jackson@jacksonargo.com>
2020-04-26 21:54:41 -0700

    remove the discord stuff since its in #106

--- pkg/api/login.go
+++ pkg/api/login.go
@@ -1,10 +1,8 @@
 package api
 
 import (
-	"errors"
 	"github.com/sirupsen/logrus"
 	"github.com/virtual-vgo/vvgo/pkg/access"
-	"github.com/virtual-vgo/vvgo/pkg/discord"
 	"github.com/virtual-vgo/vvgo/pkg/tracing"
 	"net/http"
 )
@@ -57,69 +55,6 @@ func (x PasswordLoginHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) 
 	identity = access.Identity{
 		Kind:  access.KindPassword,
 		Roles: roles,
-	}
-	loginRedirect(ctx, w, r, x.Sessions, &identity)
-}
-
-type DiscordLoginHandler struct {
-	GuildID        discord.GuildID
-	RoleVVGOMember string
-	Sessions       *access.Store
-	Discord        *discord.Client
-}
-
-var ErrNotAMember = errors.New("not a member")
-
-func (x DiscordLoginHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	ctx, span := tracing.StartSpan(r.Context(), "discord_oauth_handler")
-	defer span.Send()
-
-	handleError := func(err error) bool {
-		if err != nil {
-			tracing.AddError(ctx, err)
-			logger.WithError(err).Error("discord authentication failed")
-			unauthorized(w)
-			return false
-		}
-		return true
-	}
-
-	// get an oauth token from discord
-	code := r.FormValue("code")
-	oauthToken, err := x.Discord.QueryOAuth(ctx, code)
-	if ok := handleError(err); !ok {
-		return
-	}
-
-	// get the user id
-	discordUser, err := x.Discord.QueryIdentity(ctx, oauthToken)
-	if ok := handleError(err); !ok {
-		return
-	}
-
-	// check if this user is in our guild
-	guildMember, err := x.Discord.QueryGuildMember(ctx, x.GuildID, discordUser.ID)
-	if ok := handleError(err); !ok {
-		return
-	}
-
-	// check that they have the member role
-	var ok bool
-	for _, role := range guildMember.Roles {
-		if role == x.RoleVVGOMember {
-			ok = true
-			break
-		}
-	}
-	if !ok {
-		handleError(ErrNotAMember)
-		return
-	}
-
-	// create the identity object
-	identity := access.Identity{
-		Kind:  access.KindDiscord,
-		Roles: []access.Role{access.RoleVVGOMember},
 	}
 	loginRedirect(ctx, w, r, x.Sessions, &identity)
 }
