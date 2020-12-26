c54ed462429f9c20ace7b94b0766ee31ad57ae59
Jackson Argo <jackson@jacksonargo.com>
2020-12-26 02:21:08 -0500

    Update parts_test.go

--- pkg/sheets/parts_test.go
+++ pkg/sheets/parts_test.go
@@ -7,10 +7,10 @@ import (
 
 func Test_valuesToParts(t *testing.T) {
 	got := valuesToParts([][]interface{}{
-		{"Project", "Score Order", "Part Name", "Sheet Music File", "Click Track File", "Pronunciation Guide", "Conductor Video", "Reference Track"},
-		{"04-between-heaven-and-earth", 33, "Suspended Cymbal", "32. Between Heaven and Earth - Suspended Cymbal.pdf", "VVGO 04 FE3H Between Heaven and Earth - CLIX Track.mp3", "", "https://www.youtube.com/watch?v=GHnk2BmAFYg", "VVGO 04 FE3H Between Heaven and Earth - Reference Track.mp3"},
-		{"04-between-heaven-and-earth", 34, "Harp", "33. Between Heaven and Earth - Harp.pdf", "VVGO 04 FE3H Between Heaven and Earth - CLIX Track.mp3", "", "https://www.youtube.com/watch?v=zBmHNarPvnA", "VVGO 04 FE3H Between Heaven and Earth - Reference Track.mp3"},
-		{"04-between-heaven-and-earth", 35, "Piano", "34. Between Heaven and Earth - Piano.pdf", "VVGO 04 FE3H Between Heaven and Earth - CLIX Track.mp3", "", "https://www.youtube.com/watch?v=zBmHNarPvnA", "VVGO 04 FE3H Between Heaven and Earth - Reference Track.mp3"},
+		{"Project", "Score Order", "Part Name", "Sheet Music File", "Click Track File", "Pronunciation Guide", "Conductor Video"},
+		{"04-between-heaven-and-earth", 33, "Suspended Cymbal", "32. Between Heaven and Earth - Suspended Cymbal.pdf", "VVGO 04 FE3H Between Heaven and Earth - CLIX Track.mp3", "", "https://www.youtube.com/watch?v=GHnk2BmAFYg"},
+		{"04-between-heaven-and-earth", 34, "Harp", "33. Between Heaven and Earth - Harp.pdf", "VVGO 04 FE3H Between Heaven and Earth - CLIX Track.mp3", "", "https://www.youtube.com/watch?v=zBmHNarPvnA"},
+		{"04-between-heaven-and-earth", 35, "Piano", "34. Between Heaven and Earth - Piano.pdf", "VVGO 04 FE3H Between Heaven and Earth - CLIX Track.mp3", "", "https://www.youtube.com/watch?v=zBmHNarPvnA"},
 	})
 
 	want := Parts{
@@ -21,7 +21,6 @@ func Test_valuesToParts(t *testing.T) {
 			SheetMusicFile: "32. Between Heaven and Earth - Suspended Cymbal.pdf",
 			ClickTrackFile: "VVGO 04 FE3H Between Heaven and Earth - CLIX Track.mp3",
 			ConductorVideo: "https://www.youtube.com/watch?v=GHnk2BmAFYg",
-			ReferenceTrack: "VVGO 04 FE3H Between Heaven and Earth - Reference Track.mp3",
 		},
 		{
 			Project:        "04-between-heaven-and-earth",
@@ -30,7 +29,6 @@ func Test_valuesToParts(t *testing.T) {
 			SheetMusicFile: "33. Between Heaven and Earth - Harp.pdf",
 			ClickTrackFile: "VVGO 04 FE3H Between Heaven and Earth - CLIX Track.mp3",
 			ConductorVideo: "https://www.youtube.com/watch?v=zBmHNarPvnA",
-			ReferenceTrack: "VVGO 04 FE3H Between Heaven and Earth - Reference Track.mp3",
 		},
 		{
 			Project:        "04-between-heaven-and-earth",
@@ -39,7 +37,6 @@ func Test_valuesToParts(t *testing.T) {
 			SheetMusicFile: "34. Between Heaven and Earth - Piano.pdf",
 			ClickTrackFile: "VVGO 04 FE3H Between Heaven and Earth - CLIX Track.mp3",
 			ConductorVideo: "https://www.youtube.com/watch?v=zBmHNarPvnA",
-			ReferenceTrack: "VVGO 04 FE3H Between Heaven and Earth - Reference Track.mp3",
 		},
 	}
 
