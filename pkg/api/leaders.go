1eef93252bbc4ee7caf657e536a5a16a7462b401
Jackson Argo <jackson@jacksonargo.com>
2021-07-13 00:01:41 -0400

    remove the hardcoded entries

--- pkg/api/leaders.go
+++ pkg/api/leaders.go
@@ -34,39 +34,5 @@ func AboutMeApi(w http.ResponseWriter, r *http.Request) {
 			showEntries = append(showEntries, entry)
 		}
 	}
-
-	showEntries = []AboutMeEntry{
-		{
-			Name:  "Brandon",
-			Title: "Executive Director",
-			Blurb: "Player of assorted brass, singer of high notes, lead of the Video Team and Communications Team, stressed grad student, and the one who came up with this crazy idea.",
-			Show:  true,
-		},
-		{
-			Name:  "Jackson",
-			Blurb: "I like to make music and code things.",
-			Show:  true,
-		},
-		{
-			Name:  "Jacob",
-			Blurb: "Making music, having fun, and making more music.",
-			Show:  true,
-		},
-		{
-			Name:  "Jerome",
-			Blurb: "I look good.",
-			Show:  true,
-		},
-		{
-			Name: "Jose",
-			Show: true,
-		},
-		{
-			Name:  "Joselyn",
-			Blurb: "Coffee.",
-			Show:  true,
-		},
-	}
-
 	jsonEncode(w, showEntries)
 }
