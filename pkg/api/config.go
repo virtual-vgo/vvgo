b08fb7977321526a3d562e66e8bfe44dcb4b4070
Jackson Argo <jackson@jacksonargo.com>
2021-08-13 00:33:35 -0400

    Smol cleanup (#298)
    * set the content type
    
    * cleanup

--- pkg/api/config.go
+++ pkg/api/config.go
@@ -12,14 +12,14 @@ var ConfigApi = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	file, err := os.Open(ConfigFile)
 	if err != nil {
-		logger.Errorf("os.Open() failed: %v", err)
+		logger.SomeMethodFailure(r.Context(), "os.Open", err)
 		http.Error(w, "internal server error", http.StatusInternalServerError)
 		return
 	}
 	defer file.Close()
 
 	if _, err := io.Copy(w, file); err != nil {
-		logger.Errorf("io.Copy() failed: %v", err)
+		logger.SomeMethodFailure(r.Context(), "io.Copy", err)
 		http.Error(w, "internal server error", http.StatusInternalServerError)
 		return
 	}
