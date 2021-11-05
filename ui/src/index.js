42d573c1a3f6fe6859bf61065aaf7db851c8dc37
Jackson Argo <jackson@jacksonargo.com>
2021-11-05 02:11:23 -0400

    update index.js

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
+import {createElement} from "react";
 import * as ReactDOM from "react-dom";
 import {App} from "./components/App";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import "@fontsource/montserrat";
 import "./style.scss";
 
-ReactDOM.render(React.createElement(App), document.querySelector("#entrypoint"));
+ReactDOM.render(createElement(App), document.querySelector("#entrypoint"));
