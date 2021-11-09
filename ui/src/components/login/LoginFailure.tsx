d2bb6437498c59aca2c417b973b727a7bb53f1aa
Jackson Argo <jackson@jacksonargo.com>
2021-11-08 22:38:09 -0500

    clean up some gnarly fmt code (#357)

--- ui/src/components/login/LoginFailure.tsx
+++ ui/src/components/login/LoginFailure.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Channels } from "../../data/discord";
+import { GuildChannel } from "../../data/discord";
 import { LinkChannel } from "../shared/LinkChannel";
 
 export const LoginFailure = () => {
@@ -9,7 +9,7 @@ export const LoginFailure = () => {
       accept the rules before logging in with Discord.
       <br />
       If you think you should be able to login, please check{" "}
-      <LinkChannel channel={Channels.GeekSquad} />.
+      <LinkChannel channel={GuildChannel.GeekSquad} />.
       <br />
       <br />
       <Link to="/login">Return to the login page.</Link>
