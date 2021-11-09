c2952ed4a8a668346118bfeb8a2c59d41dedcec5
Jackson Argo <jackson@jacksonargo.com>
2021-11-08 20:25:27 -0500

    cleanup

--- ui/src/datasets/CreditsPasta.test.ts
+++ ui/src/datasets/CreditsPasta.test.ts
@@ -5,12 +5,9 @@ import { CreditsPasta } from "./CreditsPasta";
 describe("CreditsPasta", () => {
   it("#fromApiObject", () => {
     const got = CreditsPasta.fromApiObject({
-      Name: "farfalle with sausage & asparagus",
-      mixtape: "fettuccine",
-      title: "rigatoni",
-      blurb: "rigatoni",
-      channel: "rigatoni",
-      hosts: "rigatoni",
+      WebsitePasta: "cannelloni",
+      VideoPasta: "fettuccine",
+      YoutubePasta: "rigatoni",
     });
     assert.isNotEmpty(got);
     assert.equal(got!.websitePasta, "cannelloni");
