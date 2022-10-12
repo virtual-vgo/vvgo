447228dee2f4c731fd186448c3101160fcd13c97
jhlevine97 <jhlevine97@gmail.com>
2022-10-11 18:30:41 -0700

    Fix inline format

--- src/components/login/LoginFailure.tsx
+++ src/components/login/LoginFailure.tsx
@@ -5,11 +5,10 @@ import { LinkChannel } from "../shared/LinkChannel";
 export const LoginFailure = () => {
   return (
     <p>
-      Please join our <a href="https://discord.gg/mt8EZdsrc6">Discord server</a>{" "}
-      and accept the rules before logging in with Discord.
+      Please join our <a href="https://discord.gg/mt8EZdsrc6">Discord server</a> and accept the rules before logging in
+      with Discord.
       <br />
-      If you think you should be able to login, please check{" "}
-      <LinkChannel channel={GuildChannel.GeekSquad} />.
+      If you think you should be able to login, please check <LinkChannel channel={GuildChannel.GeekSquad} />.
       <br />
       <br />
       <Link to="/login">Return to the login page.</Link>
