d294538621ccd20529fc3ec7d567c9c894901cee
jhlevine97 <jhlevine97@gmail.com>
2023-03-04 12:45:14 -0800

    Fix go test constants

--- pkg/models/projects_test.go
+++ pkg/models/projects_test.go
@@ -7,10 +7,10 @@ import (
 
 func Test_valuesToProjects(t *testing.T) {
 	got := ValuesToProjects([][]interface{}{
-		{"Name", "Title", "Parts Released", "Parts Archived", "Season", "Submission Link", "Submission Deadline", "Sources", "Composers", "Arrangers", "Editors/Adaptors", "Transcribers", "Preparers", "Clix By", "Reviewers", "Lyricists", "Additional Content", "Reference Track", "Choir Pronunciation Guide", "Youtube Link", "Banner Link", "Youtube Embed", "Bandcamp Embed"},
-		{"01-snake-eater", "Snake Eater", true, true, "", "", "", "Metal Gear Solid 3", "Norihiko Hibino (日比野 則彦)", "Edited by Jerome Landingin", "Jerome Landingin", "", "The Giggling Donkey, Inc.", "Finny Jacob Zeleny", "", "", "Brandon Harnish", "01_MSG3_Snake-Eater_VVGO_Reference.mp3", "", "https://bit.ly/vvgo01", "/images/snake-eater-title-text.png", "https://www.youtube.com/embed/HVKRro_lizk", "https://bandcamp.com/EmbeddedPlayer/album=3483061952/size=large/bgcol=333333/linkcol=9a64ff/tracklist=false/artwork=small/transparent=true/"},
-		{"02-proof-of-a-hero", "Proof of a Hero", true, true, "", "", "", "Monster Hunter", "Masato Kouda (甲田 雅人)", "Arranged by Jacob Zeleny", "", "Jacob Zeleny", "The Giggling Donkey, Inc., Thomas Håkanson", "Jacob Zeleny", "Brandon Harnish", "", "", "02_MH_Proof-of-a-Hero_Reference-Track_W-CLIX", "", "https://bit.ly/vvgo2", "/images/Site_Banner_-_Proof_of_a_Hero.png", "https://www.youtube.com/embed/GJZtTe7Ayks", ""},
-		{"03-the-end-begins-to-rock", "The End Begins (To Rock)", true, true, "", "", "", "God of War II & Guitar Hero III", "Gerard K. Marino", "Orch. Shota Nakama; Additional Orch. & Arr. Thomas Håkanson", "", "", "Thomas Håkanson", "Jacob Zeleny", "Brandon Harnish, Elliot McAuley, Jerome Landingin, Thomas Håkanson", "", "", "03_The-End-Begins-to-Rock_Reference-Track-NoCLIX.mp3", "", "https://bit.ly/vvgo03", "/images/VVGO_03_TEBTR_Website_Title.png", "https://www.youtube.com/embed/2V52as93SEE", ""},
+		{"Name", "Title", "Parts Released", "Parts Archived", "Mixtape", "Season", "Submission Link", "Submission Deadline", "Sources", "Composers", "Arrangers", "Editors/Adaptors", "Transcribers", "Preparers", "Clix By", "Reviewers", "Lyricists", "Additional Content", "Reference Track", "Choir Pronunciation Guide", "Youtube Link", "Banner Link", "Youtube Embed", "Bandcamp Embed"},
+		{"01-snake-eater", "Snake Eater", true, true, false, "", "", "", "Metal Gear Solid 3", "Norihiko Hibino (日比野 則彦)", "Edited by Jerome Landingin", "Jerome Landingin", "", "The Giggling Donkey, Inc.", "Finny Jacob Zeleny", "", "", "Brandon Harnish", "01_MSG3_Snake-Eater_VVGO_Reference.mp3", "", "https://bit.ly/vvgo01", "/images/snake-eater-title-text.png", "https://www.youtube.com/embed/HVKRro_lizk", "https://bandcamp.com/EmbeddedPlayer/album=3483061952/size=large/bgcol=333333/linkcol=9a64ff/tracklist=false/artwork=small/transparent=true/"},
+		{"02-proof-of-a-hero", "Proof of a Hero", true, true, false, "", "", "", "Monster Hunter", "Masato Kouda (甲田 雅人)", "Arranged by Jacob Zeleny", "", "Jacob Zeleny", "The Giggling Donkey, Inc., Thomas Håkanson", "Jacob Zeleny", "Brandon Harnish", "", "", "02_MH_Proof-of-a-Hero_Reference-Track_W-CLIX", "", "https://bit.ly/vvgo2", "/images/Site_Banner_-_Proof_of_a_Hero.png", "https://www.youtube.com/embed/GJZtTe7Ayks", ""},
+		{"03-the-end-begins-to-rock", "The End Begins (To Rock)", true, true, false, "", "", "", "God of War II & Guitar Hero III", "Gerard K. Marino", "Orch. Shota Nakama; Additional Orch. & Arr. Thomas Håkanson", "", "", "Thomas Håkanson", "Jacob Zeleny", "Brandon Harnish, Elliot McAuley, Jerome Landingin, Thomas Håkanson", "", "", "03_The-End-Begins-to-Rock_Reference-Track-NoCLIX.mp3", "", "https://bit.ly/vvgo03", "/images/VVGO_03_TEBTR_Website_Title.png", "https://www.youtube.com/embed/2V52as93SEE", ""},
 	})
 
 	want := Projects{
@@ -19,7 +19,7 @@ func Test_valuesToProjects(t *testing.T) {
 			Title:             "Snake Eater",
 			PartsReleased:     true,
 			PartsArchived:     true,
-			Mixtape: false,
+			Mixtape:           false,
 			Sources:           "Metal Gear Solid 3",
 			Composers:         "Norihiko Hibino (日比野 則彦)",
 			Arrangers:         "Edited by Jerome Landingin",
@@ -77,7 +77,7 @@ func TestProjects_Query(t *testing.T) {
 			Title:         "Snake Eater",
 			PartsReleased: true,
 			PartsArchived: true,
-			Mixtape: 		false,
+			Mixtape:       false,
 			YoutubeLink:   "https://bit.ly/vvgo01",
 			YoutubeEmbed:  "https://www.youtube.com/embed/HVKRro_lizk",
 			BannerLink:    "/images/snake-eater-title-text.png",
@@ -88,7 +88,7 @@ func TestProjects_Query(t *testing.T) {
 			Title:         "Snake Eater",
 			PartsReleased: true,
 			PartsArchived: true,
-			Mixtape:		false,
+			Mixtape:       false,
 			YoutubeLink:   "https://bit.ly/vvgo01",
 			YoutubeEmbed:  "https://www.youtube.com/embed/HVKRro_lizk",
 			BannerLink:    "/images/snake-eater-title-text.png",
@@ -98,7 +98,7 @@ func TestProjects_Query(t *testing.T) {
 			Title:         "Proof of a Hero",
 			PartsReleased: true,
 			PartsArchived: false,
-			Mixtape: 		false,
+			Mixtape:       false,
 			YoutubeLink:   "https://bit.ly/vvgo2",
 			YoutubeEmbed:  "https://www.youtube.com/embed/GJZtTe7Ayks",
 			BannerLink:    "/images/Site_Banner_-_Proof_of_a_Hero.png",
